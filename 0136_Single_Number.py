8254060d13ef091d431d803baf474ec3db1943d9
Seonghoon-Yu <75726938+Seonghoon-Yu@users.noreply.github.com>
2021-01-19 21:55:50 +0900

    Update 0136_Single_Number.py

--- 0136_Single_Number.py
+++ 0136_Single_Number.py
@@ -5,3 +5,14 @@ class Solution:
         for num in nums:
             result ^= num
         return result
+
+# dictionary를 이용한 풀이
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        dic = {}
+        for num in nums:
+            dic[num] = dic.get(num, 0) + 1
+            
+        for key, val in dic.items:
+            if val == 1:
+                return key
