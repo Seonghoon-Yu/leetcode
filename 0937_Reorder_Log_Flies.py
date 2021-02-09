446f3f87e778c4f032f7982b70399344a9c637be
Seonghoon-Yu <75726938+Seonghoon-Yu@users.noreply.github.com>
2021-02-09 15:08:40 +0900

    Create 0937_Reorder_Log_Flies.py

---
+++ 0937_Reorder_Log_Flies.py
@@ -0,0 +1,11 @@
+def reorderLogFiles(self, logs):
+    letters, digits = [], []
+    for log in logs:
+        if log.split()[1].isdigit():
+            digits.append(log)
+        else:
+            letters.append(log)
+
+    # 2개의 키를 람다 표현식으로 정렬
+    letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+    return letters + digits
