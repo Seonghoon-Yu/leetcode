239812606e86db9a28a4e8225f08b4fa2183a10c
Seonghoon-Yu <75726938+Seonghoon-Yu@users.noreply.github.com>
2021-03-13 14:42:04 +0900

    Create 0039_Combination_Sum.py

---
+++ 0039_Combination_Sum.py
@@ -0,0 +1,17 @@
+def combinationSum(self, candidates,target):
+    result=[]
+
+    def dfs(csum, index, path):
+        # 종료 조건
+        if csum < 0:
+            return
+        if csum == 0:
+            result.append(path)
+            return
+
+        # 자신 부터 하위 원소 까지의 나열 재귀 호출
+        for i in range(index, len(candidates)):
+            dfs(csum - candidates[i], i, path + [candidates[i]])
+
+    dfs(target, 0, [])
+    return result
